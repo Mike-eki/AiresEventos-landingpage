7bedb4bad761b6866dec02cbf60d48822cde5583
mike-eki <miqueas.moreno2016@gmail.com>
2025-10-01 03:20:17 -0300

    Add sections, components, images, svgs, fonts.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,27 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+
+    experimental: {
+        fonts: [
+            {
+                provider: "local",
+                name: "Parkinsans",
+                cssVariable: "--font-parkinsans",
+                variants: [
+                    {
+                        src: [
+                            "./src/fonts/Parkinsans-Regular.ttf",
+                            "./src/fonts/Parkinsans-Medium.ttf",
+                            "./src/fonts/Parkinsans-SemiBold.ttf",
+                            "./src/fonts/Parkinsans-Bold.ttf",
+                            "./src/fonts/Parkinsans-ExtraBold.ttf"
+                        ]
+                    }
+                ],
+                fallbacks: ["sans-serif"],
+            }
+        ]
+    }
+});
